04a93c74fb038b019792f0953b0cdc17aa49f742
aurelioklv <aurelioklv@gmail.com>
2023-05-03 20:07:46 +0700

    feat: add message card and ucapan modal

--- user-home.php
+++ user-home.php
@@ -31,11 +31,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   <title>Halaman Kirim Ucapan</title>
 </head>
 <body>
-  <nav class="navbar bg-body-tertiary">
+  <nav class="navbar bg-body-tertiary fixed-top">
     <div class="container-fluid">
       <div class="d-flex align-items-center gap-3">
           <img src="img/user-icon.png" alt="Logo" width="50" height="50">
@@ -44,12 +44,72 @@
       <a class="btn btn-danger" href="logout.php">log out</a>
     </div>
   </nav>
-  <h1>Yuk kirim ucapan</h1>
+  
+  <button type="button" class="btn btn-primary fixed-bottom rounded-0 py-3" data-bs-toggle="modal" data-bs-target="#ucapan-modal">Kirim Ucapan</button>
+  <!-- Modal Kirim Ucapan -->
   <form action="POST_ucapan.php" method="post">
-    <label for="ucapan">Ucapan</label>
-    <input type="text" name="content" id="ucapan">
-
-    <input type="submit" name="kirim" value="Kirim Ucapan">
+    <div class="modal fade" id="ucapan-modal" tabindex="-1" aria-labelledby="ucapan-modal-label" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="reply-modal-label">Buat Ucapan</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <textarea class="form-control" id="reply-textarea" name="content" required></textarea>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+            <button type="submit" class="btn btn-primary" name="kirim">Kirim</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </form>
+
+  <!-- Tampilkan semua pesan yang pernah dikirim beserta balasannya -->
+  <div class="container pt-5 mt-5">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+      <div class="col">
+        <div class="card">
+          <?php $isReplied = false; ?>
+          <div class="card-header <?php echo $isReplied ? 'bg-success' : 'bg-warning'; ?>">
+            <?php echo $isReplied ? 'Dibalas' : 'Belum dibalas'; ?>
+          </div>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, perferendis.</p>
+            <?php if ($isReplied) { ?>
+              <p class="card-text text-end">Balasan dari admin</p>
+            <?php }?>
+          </div>
+          <div class="card-footer text-muted">
+            Dikirim pada 3 Mei 2023
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <?php $isReplied = true; ?>
+          <div class="card-header <?php echo $isReplied ? 'bg-success' : 'bg-warning'; ?>">
+            <?php echo $isReplied ? 'Dibalas' : 'Belum dibalas'; ?>
+          </div>
+          <div class="card-body">
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, perferendis.</p>
+            <?php if (!$isReplied) { ?>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#reply-modal-<ucapan_id>">
+                  Balas
+              </button>
+            <?php } else { ?>
+              <hr>
+              <p class="card-text text-end">ingfo diterima, nice ingfo</p>
+            <?php } ?>
+          </div>
+          <div class="card-footer text-muted">
+            Dikirim pada 3 Mei 2023
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
